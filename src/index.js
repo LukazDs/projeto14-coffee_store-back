fa6be81158f3102cecaba282b716772514e6b0fa
Lucas Duarte <lukazds2016@gmail.com>
2022-07-09 15:54:56 -0300

    Conectando servidor a rota /sign-up

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from "cors";
+import authRouter from './routes/authRouter.js';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -8,6 +9,8 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.use(authRouter)
 
 app.listen(process.env.PORT, () => {
     console.log("Conexão estabelecida");
---
+++ src/schemas/userSchemas.js
@@ -0,0 +1,9 @@
+import joi from "joi";
+
+export const registerUserSchema = joi.object({
+    name: joi.string().required(),
+    email: joi.string().required(),
+    password: joi.string().required(),
+    confirmPassword: joi.string().required()
+
+});
