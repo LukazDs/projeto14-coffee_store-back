4f0e556ec312f4c9c32a77952ef0f394905726e1
Lucas Duarte <lukazds2016@gmail.com>
2022-07-10 21:00:34 -0300

    Estabelecendo conexão com o banco de dados ao logar

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,6 +1,7 @@
-import { db } from '../dbStrategy/mongo.js';
+import { db, objectId } from '../dbStrategy/mongo.js';
 import bcrypt from 'bcrypt';
-import { registerUserSchema } from "../schemas/userSchemas.js";
+import { v4 as uuid } from 'uuid';
+import { registerUserSchema, loginSchema } from "../schemas/userSchemas.js";
 
 
 export async function createUser(req, res) {
@@ -28,6 +29,36 @@ export async function createUser(req, res) {
 
         res.sendStatus(201);
 
+    } catch (error) {
+        res.status(500).send("Problema de cadastramento!");
+    }
+}
+
+export async function loginUser(req, res) {
+
+    const user = req.body;
+
+    const validation = loginSchema.validate(user);
+
+    if (validation.error) {
+        return res.status(422).send("Email ou senha invalidos!");
+    }
+
+    try {
+        const userDb = await db.collection('users').findOne({ email: user.email });
+
+        if (userDb && bcrypt.compareSync(user.password, userDb.password)) {
+            const token = uuid();
+
+            await db.collection('sessions').insertOne({
+                token,
+                userId: new objectId(userDb._id),
+            });
+
+            return res.status(201).send({ token });
+        } else {
+            return res.status(401).send('Senha ou email incorretos!');
+        }
     } catch (error) {
         res.status(500).send("Problema de cadastramento!");
     }
--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -1,8 +1,9 @@
-import { createUser } from '../controllers/authController.js';
+import { createUser, loginUser } from '../controllers/authController.js';
 import { Router } from 'express';
 
 const router = Router();
 
 router.post('/sign-up', createUser);
+router.post('/login', loginUser);
 
 export default router;
